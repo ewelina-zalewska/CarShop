4e38d317f01858ce447f7507fc0f9ceb72d60d68
Ewelina <ewe.zale@gmail.com>
2025-01-11 11:05:22 +0100

    refactor: destructure object

--- src/components/createForm/categories/NewCategory.tsx
+++ src/components/createForm/categories/NewCategory.tsx
@@ -1,4 +1,4 @@
-﻿import { FormEvent, useEffect } from "react";
+﻿import { FormEvent } from "react";
 import { categoriesQueryOptions } from "@/queries/categoriesQuery";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { useNavigate } from "@tanstack/react-router";
@@ -6,49 +6,60 @@ import { CategoryForm } from "@/types";
 import { useForm } from "@/hooks/useForm";
 import { useCreateCategoryMutation } from "@/mutations/useCreateCategoryMutation";
 import { NewCategoryFormFieldset } from "@/components/createForm/categories/NewCategoryFormFieldset";
+import { TheButton } from "@/Shared/TheButton";
 
 export const NewCategory = () => {
-	const { data } = useSuspenseQuery(categoriesQueryOptions);
+	const { data: categories } = useSuspenseQuery(categoriesQueryOptions);
 	const { mutate: CREATE_CATEGORY, isSuccess } = useCreateCategoryMutation();
-
 	const [formState, setFormState, HANDLE_CHANGE] = useForm<CategoryForm>({
 		name: "",
 		identifier: "",
 	});
+	const { name, identifier } = formState;
 
 	const HANDLE_SUBMIT = (e: FormEvent) => {
 		e.preventDefault();
 
 		CREATE_CATEGORY({
-			name: formState.name,
-			identifier: formState.identifier,
-			position: data.length + 1,
+			name,
+			identifier,
+			position: categories.length + 1,
 		});
 
 		setFormState({
-			name: "",
-			identifier: "",
+			name,
+			identifier,
 		});
 	};
 
 	const navigate = useNavigate();
-	useEffect(() => {
-		if (!isSuccess) return;
-		navigate({ to: "/options/category" });
-	}, [isSuccess]);
 
+	const GO_TO_CREATE_PART = () => {
+		const lastCategory = categories[categories.length - 1];
+		navigate({ to: `/options/category/${lastCategory.id}/newPart` });
+	};
 	return (
 		<>
 			<h1>STWÓRZ NOWĄ KATEGORIĘ</h1>
 			<div>
-				<h2>New category</h2>
+				<h2>Nowa kategoria</h2>
 				<form onSubmit={HANDLE_SUBMIT}>
 					<NewCategoryFormFieldset
 						onChange={HANDLE_CHANGE}
 						formState={formState}
 					/>
-					<button type="submit">DODAJ</button>
+					<TheButton
+						disabled={isSuccess}
+						btnLabel="DODAJ KATEGORIĘ"
+						type="submit"
+					></TheButton>
 				</form>
+				{isSuccess && (
+					<div>
+						<p>Kategoria {name} została dodana. Dodaj opcję. </p>
+						<button onClick={GO_TO_CREATE_PART}>DODAJ OPCJĘ</button>
+					</div>
+				)}
 			</div>
 		</>
 	);
