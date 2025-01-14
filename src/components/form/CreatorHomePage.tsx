cbb5bca0ea38a5413347ec1fb4d35f63cf150696
Ewelina <ewe.zale@gmail.com>
2025-01-14 16:39:02 +0100

    fix: update creator component after refreshing the page, refactor code

--- src/components/form/CreatorHomePage.tsx
+++ src/components/form/CreatorHomePage.tsx
@@ -1,15 +1,15 @@
-﻿import { categoriesQueryOptions } from "@/queries/categoriesQuery";
-import { TheButton } from "@/Shared/TheButton";
-import { CategoryResponse } from "@/types";
+﻿import { useEffect, useState } from "react";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Outlet, useNavigate } from "@tanstack/react-router";
-import { useEffect, useState } from "react";
+import { categoriesQueryOptions } from "@/queries/categoriesQuery";
+import { CategoryResponse } from "@/types";
+import { useGetStarted } from "@/hooks/useGetStarted";
+import { TheButton } from "@/Shared/TheButton";
 
 export const CreatorHomePage = () => {
-	const [firstCategory, setFirstCategory] = useState<CategoryResponse>();
-	const [started, setStarted] = useState<boolean>(false);
-
 	const { data: categories } = useSuspenseQuery(categoriesQueryOptions);
+	const [firstCategory, setFirstCategory] = useState<CategoryResponse>();
+	const form = useGetStarted();
 
 	useEffect(() => {
 		const category = categories.find((category) => category.position === 1);
@@ -21,25 +21,29 @@ export const CreatorHomePage = () => {
 	if (!firstCategory) return <p> No form ... </p>;
 
 	const GO_TO_FORM = () => {
-		if (started) {
+		if (form.isStarted) {
 			navigate({ to: `/` });
-			setStarted(false);
+			localStorage.setItem("form", "closed");
+			form.setForm("closed");
 			return;
 		}
 		navigate({ to: `/creator/${firstCategory.id}` });
-		setStarted(true);
+		localStorage.setItem("form", "started");
+		form.setForm("started");
 	};
 
-	const btnLabel = started ? "STOP" : "START";
 	return (
 		<>
-			{!started && (
+			{form.isClosed && (
 				<div>
 					<h1>Kreator auta</h1>
 					<p>Przejdź do formularza klikając na start</p>
 				</div>
 			)}
-			<TheButton btnLabel={btnLabel} onClick={GO_TO_FORM} />
+			<TheButton
+				btnLabel={form.isStarted ? "STOP" : "START"}
+				onClick={GO_TO_FORM}
+			/>
 			<Outlet />
 		</>
 	);
---
+++ src/hooks/useGetStarted.ts
@@ -0,0 +1,18 @@
+﻿import { useEffect, useState } from "react";
+
+export const useGetStarted = () => {
+	const [form, setForm] = useState<"started" | "closed">("closed");
+
+	useEffect(() => {
+		const isStarted = localStorage.getItem("form");
+		if (isStarted === "started" || isStarted === "closed") {
+			setForm(isStarted);
+		}
+	}, []);
+
+	return {
+		isStarted: form === "started",
+		isClosed: form === "closed",
+		setForm,
+	};
+};
