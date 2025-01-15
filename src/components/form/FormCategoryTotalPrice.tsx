f42757c49878c8b21e24893cd8b3db1ca22c95b8
Ewelina <ewe.zale@gmail.com>
2025-01-15 16:28:17 +0100

    refactor: create useSelectedParts

--- src/components/form/FormCategoryTotalPrice.tsx
+++ src/components/form/FormCategoryTotalPrice.tsx
@@ -1,34 +1,9 @@
-﻿import { useEffect, useState } from "react";
-import { useShallow } from "zustand/shallow";
-import { selectedPart } from "@/types";
-import { useFormStore } from "@/store/useFormStore";
-import { useTotalprice } from "@/hooks/useTotalPrice";
+﻿import { useTotalprice } from "@/hooks/useTotalPrice";
+import { useSelectedParts } from "@/hooks/useSelectedParts";
 
 export const FormCategoryTotalPrice = () => {
-	const [selectedParts, setSelectedParts] = useState<selectedPart[]>([]);
-
-	const { form } = useFormStore(
-		useShallow((state) => ({
-			form: state.form,
-		})),
-	);
-
+	const selectedParts = useSelectedParts();
 	const totalPrice = useTotalprice(selectedParts);
-
-	useEffect(() => {
-		const partsToAdd: selectedPart[] = [];
-		for (let i = 0; i < localStorage.length; i++) {
-			const key = localStorage.key(i);
-			if (key && key !== "form") {
-				const selectParts = localStorage.getItem(key);
-				if (selectParts) {
-					const selectedPartsParsed: selectedPart = JSON.parse(selectParts);
-					partsToAdd.push(selectedPartsParsed);
-				}
-			}
-		}
-		setSelectedParts(partsToAdd);
-	}, [form.partValue]);
 
 	return (
 		<h3>
---
+++ src/hooks/useSelectedParts.ts
@@ -0,0 +1,31 @@
+﻿import { useFormStore } from "@/store/useFormStore";
+import { selectedPart } from "@/types";
+import { useEffect, useState } from "react";
+import { useShallow } from "zustand/shallow";
+
+export const useSelectedParts = () => {
+	const [selectedParts, setSelectedParts] = useState<selectedPart[]>([]);
+
+	const { form } = useFormStore(
+		useShallow((state) => ({
+			form: state.form,
+		})),
+	);
+
+	useEffect(() => {
+		const partsToAdd: selectedPart[] = [];
+		for (let i = 0; i < localStorage.length; i++) {
+			const key = localStorage.key(i);
+			if (key && key !== "form") {
+				const selectParts = localStorage.getItem(key);
+				if (selectParts) {
+					const selectedPartsParsed: selectedPart = JSON.parse(selectParts);
+					partsToAdd.push(selectedPartsParsed);
+				}
+			}
+		}
+		setSelectedParts(partsToAdd);
+	}, [form.partValue]);
+
+	return selectedParts;
+};
