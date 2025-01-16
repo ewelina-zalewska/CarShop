2029a9d49dd2835ab985e5213c3e16d1c4e349da
Ewelina <ewe.zale@gmail.com>
2025-01-16 10:42:14 +0100

    feat: create common component

---
+++ src/Shared/TheInput.tsx
@@ -0,0 +1,32 @@
+﻿import { FormChangeEvent } from "@/types";
+
+type TheInputProps = {
+	legend: string;
+	type: string;
+	name: string;
+	placeholder: string;
+	value: string | number;
+	onChange: (e: FormChangeEvent) => void;
+};
+
+export const TheInput = ({
+	legend,
+	type,
+	name,
+	placeholder,
+	value,
+	onChange,
+}: TheInputProps) => {
+	return (
+		<div>
+			<legend>{legend}</legend>
+			<input
+				type={type}
+				name={name}
+				placeholder={placeholder}
+				value={value}
+				onChange={onChange}
+			/>
+		</div>
+	);
+};
