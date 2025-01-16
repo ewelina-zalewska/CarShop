df7afb83b6f2a2fd81cbeca7c67c9b5398d438a8
Ewelina <ewe.zale@gmail.com>
2025-01-16 10:34:37 +0100

    feat: show changing categories

---
+++ src/components/form/TheStepper.tsx
@@ -0,0 +1,26 @@
+﻿import { CategoryResponse } from "@/types";
+
+type TheStepperProps = {
+	categories: CategoryResponse[];
+	selectedCategory: string;
+};
+
+export const TheStepper = ({
+	categories,
+	selectedCategory,
+}: TheStepperProps) => {
+	return (
+		<ul>
+			{categories.map((category) => (
+				<li
+					key={category.id}
+					style={{
+						fontWeight: `${category.id === selectedCategory ? "bold" : "normal"}`,
+					}}
+				>
+					{category.name}
+				</li>
+			))}
+		</ul>
+	);
+};
