865eb2072fc2a43128b12bc6b669273ca2776a28
Ewelina <ewe.zale@gmail.com>
2025-01-16 10:41:10 +0100

    feat: create form store

---
+++ src/store/useFormStore.ts
@@ -0,0 +1,29 @@
+﻿import { create } from "zustand";
+
+type FormState = {
+	form: {
+		partValue: string;
+	};
+};
+
+type FormActions = {
+	setFormData: (payload: setFormDataAction) => void;
+};
+
+type setFormDataAction = {
+	partValue: string;
+};
+
+const initialState: FormState = {
+	form: {
+		partValue: "",
+	},
+};
+
+export const useFormStore = create<FormState & FormActions>()((set) => ({
+	...initialState,
+	setFormData: (payload: setFormDataAction) =>
+		set({
+			form: payload,
+		}),
+}));
