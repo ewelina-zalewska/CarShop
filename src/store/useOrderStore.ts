d447229c869bb8b2a8316ca8217b20e4e36a1c57
Ewelina <ewe.zale@gmail.com>
2025-01-17 19:28:56 +0100

    add order mode value

---
+++ src/store/useOrderStore.ts
@@ -0,0 +1,48 @@
+﻿import { create } from "zustand";
+
+type FormState = {
+	form: {
+		partValue: string;
+	};
+	order: {
+		orderMode: "show" | "hide";
+	};
+};
+
+type FormActions = {
+	setFormData: (payload: setFormDataAction) => void;
+};
+type OrderActions = {
+	setOrderData: (payload: setOrderDataAction) => void;
+};
+
+type setFormDataAction = {
+	partValue: string;
+};
+
+type setOrderDataAction = {
+	orderMode: "show" | "hide";
+};
+
+const initialState: FormState = {
+	form: {
+		partValue: "",
+	},
+	order: {
+		orderMode: "hide",
+	},
+};
+
+export const useOrderStore = create<FormState & FormActions & OrderActions>()(
+	(set) => ({
+		...initialState,
+		setFormData: (payload: setFormDataAction) =>
+			set({
+				form: payload,
+			}),
+		setOrderData: (payload: setOrderDataAction) =>
+			set({
+				order: payload,
+			}),
+	}),
+);
