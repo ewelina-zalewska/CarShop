2bba13d5f22ef2d4d61a1e7d71397e1b1ef6969e
Ewelina <ewe.zale@gmail.com>
2025-01-24 13:56:56 +0100

    feat: get part

---
+++ src/queries/partQuery.ts
@@ -0,0 +1,11 @@
+﻿import { PartsResponse } from "@/types";
+import { apiCall } from "@/utils/apiCall";
+import { queryOptions } from "@tanstack/react-query";
+
+export const partQueryOptions = (id: string) =>
+	queryOptions({
+		queryKey: ["category", id],
+		queryFn: async () => {
+			return apiCall<PartsResponse>(`parts/${id}`);
+		},
+	});
